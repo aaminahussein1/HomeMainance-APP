46521c08308c389f1f59da50c1893f58c7f67a74
ahmed <abdifatahismail886@gmail.com>
2026-02-10 14:24:47 +0300

    Settup completed for the Mtin Mtintenance Atpp

--- Backend/server.js
+++ Backend/server.js
@@ -1,34 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
 
-const connectDB = require('./config/db');
-const errorHandler = require('./middleware/error');
-const reviewsRoutes = require('./routes/reviews');
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db.js';
+import serviceRoutes from './routes/services.js';
+
+dotenv.config();
+connectDB();
 
 const app = express();
 
-connectDB();
-
-
+app.use(cors());
 app.use(express.json());
-app.use(cors({
-  origin: ['http://localhost:3000', process.env.FRONTEND_URL],
-  credentials: true
-}));
 
+app.use('/api/services', serviceRoutes);
 
-app.get('/api/test', (req, res) => {
-  res.json({ message: "API WORKING " });
+app.get('/', (req, res) => {
+  res.send('HOME Maintenance API running...');
 });
-
-
-app.use('/api/reviews', reviewsRoutes);
-
-
-app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
