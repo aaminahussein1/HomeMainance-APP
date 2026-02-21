ce51f23505fa6cf329928197f0fb56dd0f42aa85
Aamina Hussain <aaminahussein167@gmail.com>
2026-02-21 16:30:07 +0300

    Fix build scripts for production

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5006/api',
+  // Haddii uu mashruucu Live yahay, wuxuu isticmaalayaa '/api' 
+  // Haddii aad kombiyuutarkaaga ku jirto (localhost), wuxuu isticmaalayaa 'http://localhost:5006/api'
+  baseURL: window.location.origin.includes('localhost') 
+    ? 'http://localhost:5006/api' 
+    : '/api',
 });
 
 api.interceptors.request.use((config) => {
